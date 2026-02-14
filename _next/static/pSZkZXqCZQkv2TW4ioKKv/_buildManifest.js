3832cb1eeb48e942163dffe848956e3808aead86
dylan <dylanmartin1922@gmail.com>
2026-02-14 18:36:25 -0300

    Updates

--- _next/static/pSZkZXqCZQkv2TW4ioKKv/_buildManifest.js
+++ _next/static/pSZkZXqCZQkv2TW4ioKKv/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/gestxr.github.io//_next/:path+",
-        "destination": "/gestxr.github.io/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
