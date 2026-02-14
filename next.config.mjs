fe79e840614620c95822e0715b5b48aa0a0d90d5
dylan <dylanmartin1922@gmail.com>
2026-02-14 18:36:02 -0300

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'export', // <=== enables static exports
-  // Optional: Configure basePath and assetPrefix for the GitHub Pages subpath URL
-  basePath: '/gestxr.github.io',
-  assetPrefix: '/gestxr.github.io/',
   distDir: 'build'
 };
 
