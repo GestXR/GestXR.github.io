8eae61da91493629a630d72a3a636f00f353799c
dylan <dylanmartin1922@gmail.com>
2026-02-24 22:07:19 -0300

    test

--- src/app/components/VideoEmbed.js
+++ src/app/components/VideoEmbed.js
@@ -10,7 +10,7 @@ export default function VideoEmbed({c}) {
         classNameNotInView="opacity-0"
         percentage={0}
       >
-        <iframe className="rounded-xl w-full aspect-video outline-[1px] outline-zinc-950" src={c.fuente} title={c.nobmre} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe className="rounded-xl w-full aspect-video outline-[1px] outline-zinc-950" src={c.fuente} title={c.nobmre || "video"} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </AnimationOnScroll>
     </div >
   )
